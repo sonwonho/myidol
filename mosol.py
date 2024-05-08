79c8262d56dc3396219dce7da7b5c9a07d2ef31a
Ho <sonwonho2005@gmail.com>
2024-05-08 11:53:28 +0900

    Update port

--- mosol.py
+++ mosol.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -31,6 +32,16 @@ def make_image(select: SelectItem):
     prompt, negative_prompt = prompt_mapper.make_prompt(prompt_data_list)
     image_url = karlo.get_image_url(prompt, negative_prompt)
     select_dict = select.model_dump()
+    select_dict.pop("hobby1", None)
+    select_dict.pop("hobby2", None)
+    hobby = select_dict.pop("hobbyList", None)
+    if hobby:
+        hobby = ", ".join(hobby)
+    select_dict.update({"hobby": hobby})
     select_dict.update({"picture": image_url})
     result = {"code": "200", "data": select_dict, "message": "OK"}
     return result
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="mosol:app", host="0.0.0.0", port=5000, reload=False)
