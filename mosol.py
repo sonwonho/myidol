da38e5e62ad7c4bc82d3c93e740040a263a2c09c
Ho <sonwonho2005@gmail.com>
2024-05-08 18:00:46 +0900

    Add swagger example

--- mosol.py
+++ mosol.py
@@ -24,9 +24,87 @@ class SelectItem(BaseModel):
     hobby1: Optional[str]
     hobby2: Optional[str]
     hobbyList: list
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "age": "10대",
+                    "sex": "남성",
+                    "mbti": "ISTJ",
+                    "lookLike": "홍길동",
+                    "height": "156cm ~ 160cm",
+                    "eyeShape": "무쌍 실눈",
+                    "faceShape": "강아지상",
+                    "fashion": "미니멀",
+                    "hobby1": "영화",
+                    "hobby2": "축구",
+                    "hobbyList": ["축구", "농구"],
+                }
+            ]
+        }
+    }
 
 
-@app.post("/make_picture")
+class PictureItem(BaseModel):
+    age: Optional[str]
+    sex: str
+    mbti: Optional[str]
+    lookLike: Optional[str]
+    height: Optional[str]
+    eyeShape: str
+    faceShape: str
+    fashion: Optional[str]
+    hobby: str
+    picture: str
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "age": "10대",
+                    "sex": "남성",
+                    "mbti": "ISTJ",
+                    "lookLike": "홍길동",
+                    "height": "156cm ~ 160cm",
+                    "eyeShape": "무쌍 실눈",
+                    "faceShape": "강아지상",
+                    "fashion": "미니멀",
+                    "hobby": "축구, 농구",
+                    "picture": "https://mk.kakaocdn.net/dna/karlo/image/yyyy-mm-dd/...",
+                }
+            ]
+        }
+    }
+
+
+class ResultItem(BaseModel):
+    code: str
+    data: Optional[PictureItem]
+    message: str
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "code": "200",
+                    "data": {
+                        "age": "10대",
+                        "sex": "남성",
+                        "mbti": "ISTJ",
+                        "lookLike": "홍길동",
+                        "height": "156cm ~ 160cm",
+                        "eyeShape": "무쌍 실눈",
+                        "faceShape": "강아지상",
+                        "fashion": "미니멀",
+                        "hobby": "축구, 농구",
+                        "picture": "https://mk.kakaocdn.net/dna/karlo/image/yyyy-mm-dd/...",
+                    },
+                    "message": "OK",
+                }
+            ]
+        }
+    }
+
+
+@app.post("/make_picture", response_model=ResultItem)
 def make_image(select: SelectItem):
     prompt_data_list = [select.sex, select.eyeShape, select.faceShape]
     prompt, negative_prompt = prompt_mapper.make_prompt(prompt_data_list)
